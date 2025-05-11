36a09409c9a6f84f7467493fc89413f30c9cc345
raushan778 <allmusiccenter404@gmail.com>
2025-05-11 10:59:44 +0530

    Vite Config

--- admin-dashboard/vite.config.js
+++ admin-dashboard/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Admin-Dashboard',
+  base: '/Admin-Dashboard/index.html',
   plugins: [
     react(),
     tailwindcss(),
